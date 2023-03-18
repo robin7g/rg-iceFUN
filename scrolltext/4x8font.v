9743e12732b91463479c11fce005f37e1c432df8
Robin Grosset <robin@grosset.net>
2023-03-17 22:11:49 -0400

    Initial check-in
    Scrolling text example using a 4x8 LED matrix unique to iceFUN

---
+++ scrolltext/4x8font.v
@@ -0,0 +1,79 @@
+/*
+ *  Scroll Text Display using a 4x8 Font.  
+ * 
+ *  Copyright(C) 2022 Robin Grosset
+ *  
+ *  This file and the entire 4x8 font data created for this project 
+ *  is shared under the MIT license 
+ * 
+ */ 
+
+
+// This is a 4x8 font for used with iceFUN pixel matrix
+// its a very awkward font size
+
+parameter  _s = 8'h00; // single line space
+parameter  _cSP = 32'h00000000; // space
+parameter  _cEX = 32'h00005F00; // explamation !
+parameter  _cQQ = 32'h02015906; // question ?
+parameter  _cFS = 32'h00400000; // full stop . 
+parameter  _cCO = 32'h00002400; // colon : 
+parameter  _cSC = 32'h00402400; // semi colon ; 
+
+parameter  _cPLUS      = 32'h00081C08; // plus + 
+parameter  _cMINUS     = 32'h00080808; // minus - 
+parameter  _cDIVIDE    = 32'h40300C02; // divide / 
+parameter  _cMULTIPLY  = 32'h62142846; // multiply x 
+parameter  _cEQUALS    = 32'h14141414; // equals = 
+
+parameter  _cFWDSLASH  = 32'h020C3040; // fwd slash \
+parameter  _cOPENSQ    = 32'h00FF8100; // open sq [
+parameter  _cCLOSESQ   = 32'h0081FF00; // close sq ]
+parameter  _cFWDSLASH  = 32'h020C3040; // fwd slash \
+parameter  _cOPENBR    = 32'h3C428100; // open br (
+parameter  _cCLOSEBR   = 32'h0081423C; // close br )
+
+// Missing Stuff! 
+//  ' " $ # @ ^ % & | { }
+
+// Hex Characters
+parameter  _c0 = 32'h7E91897E;
+parameter  _c1 = 32'h0002FF00;
+parameter  _c2 = 32'hF2898986;
+parameter  _c3 = 32'h42898976;
+parameter  _c4 = 32'h000C0AFF;
+parameter  _c5 = 32'h4F898971;
+parameter  _c6 = 32'h7E898972;
+parameter  _c7 = 32'h01E11907;
+parameter  _c8 = 32'h76898976;
+parameter  _c9 = 32'h4689897E;
+parameter  _cA = 32'hFE0909FE;
+parameter  _cB = 32'hFF898976;
+parameter  _cC = 32'h7E818142;
+parameter  _cD = 32'hFF81817E;
+parameter  _cE = 32'hFF898981; 
+parameter  _cF = 32'hFF090901;
+
+// Rest of Alphabet
+parameter  _cG = 32'h7E819172;
+parameter  _cH = 32'hFF0808FF;
+parameter  _cI = 32'h0081FF81;
+parameter  _cJ = 32'h41817F01;
+parameter  _cK = 32'hFF0814E3;
+parameter  _cL = 32'hFF808080;
+parameter  _cM = 32'hFF0202FF;
+parameter  _cN = 32'hFF0810FF;
+parameter  _cO = 32'h7E81817E;
+parameter  _cP = 32'hFF090906;
+parameter  _cQ = 32'h3E41C1BE;
+parameter  _cR = 32'hFF1929B2;
+parameter  _cS = 32'h46849162;
+parameter  _cT = 32'h01FF0101;
+parameter  _cU = 32'h7F80807F;
+parameter  _cV = 32'h3F40807F;
+parameter  _cW = 32'hFF4040FF;
+parameter  _cX = 32'hEF1010EF;
+parameter  _cY = 32'h0304F807;
+parameter  _cZ = 32'hE1918987;
+
+
---
+++ scrolltext/scrolltext.v
@@ -0,0 +1,105 @@
+/*
+ *  Scroll Text Display using a 4x8 Font.  
+ * 
+ *  Copyright(C) 2022 Robin Grosset
+ *  
+ *  This file and the entire 4x8 font data created for this project 
+ *  is shared under the MIT license 
+ *  
+ *  Led scan code is reused from an example by Gerald Coe, Devantech Ltd
+ *  his copyright and permissive license inclued in ledscan.v
+ * 
+ */
+ 
+`include "ledscan.v"
+`include "4x8font.v" 
+
+module top (
+	input clk12MHz, 
+	output led1, 
+	output led2, 
+	output led3, 
+	output led4, 
+	output led5, 
+	output led6, 
+	output led7, 
+	output led8, 
+	output lcol1, 
+	output lcol2, 
+	output lcol3, 
+	output lcol4
+	);
+
+	// Led pixel registers, ledscan.v ensures these are flashed to the display one by one
+	reg [7:0] leds1;
+	reg [7:0] leds2;
+	reg [7:0] leds3;
+	reg [7:0] leds4;
+	
+
+
+	// Interface code to ledscan.v module. 
+	wire [7:0] leds;
+	wire [3:0] lcol;
+
+	// all the stuff wired up. 
+	assign { led8, led7, led6, led5, led4, led3, led2, led1 } = leds[7:0];
+	assign { lcol4, lcol3, lcol2, lcol1 } = lcol[3:0];
+	
+	// a 32 bit counter used to scroll the text
+    reg [31:0] counter = 32'b0;
+
+	// Gerald's led scan module this manages the display logic to mutiplex the 
+	// led control lines. Like a matrix keyboard but in reverse. 
+ 	LedScan scan (
+		.clk12MHz(clk12MHz), 
+		.leds1(leds1),		
+		.leds2(leds2),
+		.leds3(leds3),
+		.leds4(leds4),
+		.leds(leds), 
+		.lcol(lcol)
+	);
+
+
+    // This is count of vertial lines in the entire set of characters. 
+	// we are going to scroll the lines across the screen 
+	localparam lines = 65;
+	reg [7:0] line_count = 65;
+	
+	// _ H E L L O _ W O R L D ! _ 
+	// Chars = 4 wide.. Single Line Space = 1 wide
+	// 4, 4, 1, 4, 1, 4, 1, 4, 1, 4, 4, 4, 1, 4, 1, 4, 1, 4, 1, 4, 1, 4, 4
+	// Sum = 65 lines = 520 bits
+
+	reg [520:0] hex = {_cSP,_cH,_s,_cE,_s, _cL,_s, _cL,_s, _cO, _cSP,
+			_cW,_s,  _cO,_s,  _cR, _s, _cL, _s, _cD, _s, _cEX, _cSP};
+
+	
+   
+	// Here we select data to display on the 'screen' of leds
+	// each 8 leds is a line. 
+    always @ (*) begin
+		leds1[7:0] = ~ hex[line_count*8    : line_count*8-8]; 
+		leds2[7:0] = ~ hex[line_count*8-9  : line_count*8-16]; 
+		leds3[7:0] = ~ hex[line_count*8-17 : line_count*8-24]; 
+		leds4[7:0] = ~ hex[line_count*8-25  : line_count*8-32]; 
+    end
+
+	// increment the counter every clock and after a certain number of clocks
+	// we shift the pixel lines along. 	
+    always @ (posedge clk12MHz) begin
+        counter <= counter + 1;
+		if (counter > 'h18FFFF) begin
+			// This condition should be reached after ~100 ms. 
+			// so one character of 5 pixels wide  (includes the blank spacer) 
+			// moves across the pixel grid in ~ 1/2 a second
+			line_count <= line_count - 1;
+			if (line_count == 4 ) begin
+				line_count <= lines;
+			end	
+			counter <= 0;
+		end 
+    end
+
+endmodule
